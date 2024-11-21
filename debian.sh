b65acf01c82b64de1840b5bb36cbee4c6e852813
kieran <kieran@harkin.me>
2024-11-21 12:40:34 +0000

    fix: ci

--- debian.sh
+++ debian.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
-apt update && apt install -y \
-      build-essential \
-      pkg-config \
-      libx264-dev \
-      libwebp-dev \
-      libssl-dev \
-      libpipewire-0.3-dev \
-      libpulse-dev \
-      libpng-dev \
-      libasound2-dev \
-      libclang-dev \
-      nasm
+apt update && \
+    apt install -y \
+    build-essential \
+    libx264-dev \
+    libx265-dev \
+    libwebp-dev \
+    libvpx-dev \
+    libopus-dev \
+    libdav1d-dev \
+    libpipewire-0.3-dev \
+    libasound2-dev \
+    nasm \
+    libclang-dev \
+    squashfs-tools
 git clone --single-branch --branch release/7.1 https://git.v0l.io/ffmpeg/FFmpeg.git && \
     cd FFmpeg && \
     ./configure \
+    --prefix=${FFMPEG_DIR} \
     --disable-programs \
     --disable-doc \
+    --disable-network \
     --enable-gpl \
     --enable-libx264 \
+    --enable-libx265 \
     --enable-libwebp \
+    --enable-libvpx \
+    --enable-libopus \
+    --enable-libdav1d \
+    --disable-static \
     --disable-postproc \
-    --enable-static \
-    --disable-shared && \
+    --enable-shared && \
     make -j$(nproc) install
-export CARGO_FEATURE_STATIC=1
+
 cargo install xbuild
-x build --release
+x build --release --format appimage
